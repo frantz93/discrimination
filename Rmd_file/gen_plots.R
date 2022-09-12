e4791e8b3b0194b2b421d576c4d6bcc317b0f3e1
frantz93 <frantzroby@gmail.com>
2022-09-12 09:39:27 -0400

    .

---
+++ Rmd_file/gen_plots.R
@@ -0,0 +1,41 @@
+#Save graphs commands
+
+#save p0
+png(file = "plot/supp_var_mca.png", width = 1280/2, height = 740/2)
+p0
+dev.off()
+
+#save p1
+png(file = "plot/main_var_mca.png", width = 1280/2, height = 740/2)
+p1
+dev.off()
+
+#save p2
+png(file = "plot/wage_boxplot.png", width = 1280/2, height = 740/2)
+p2
+dev.off()
+
+#save p3
+png(file = "plot/wage_density.png", width = 1280/2, height = 740/2)
+p3
+dev.off()
+
+#save p4
+png(file = "plot/unemp_prop.png", width = 1280/2, height = 740/2)
+p4
+dev.off()
+
+#save p5
+png(file = "plot/id_wagegrowth.png", width = 1280/2, height = 740/2)
+p5
+dev.off()
+
+#save p6
+png(file = "plot/educ_wagegrowth.png", width = 1280/2, height = 740/2)
+p6
+dev.off()
+
+#save p7
+png(file = "plot/occup_wagegrowth.png", width = 1500/2, height = 740/2)
+p7
+dev.off()
